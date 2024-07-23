4db6126477829e4931c31de1b77d55ffcca14175
Norbert M. Soto <norbert.soto@sunnova.com>
2024-07-23 17:52:31 -0400

    Map

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+'use client';
 
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+    <MapContainer
+    center={{
+      lat: 18.37733398653396,
+      lng: -65.92457771301271,
+    }}
+    zoom={10}
+    style={{ 
+      height: '500px',
+      width: '800px',
+      margin: 'auto',
+    }}
+    >
+    <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+    </div>
+  );
 }
+
