3b5885ceecf1f58bbf84ca601ada43c6cad7dfc5
Norbert M. Soto <norbert.soto@sunnova.com>
2024-07-23 17:55:19 -0400

    Revert "Map"
    This reverts commit 4db6126477829e4931c31de1b77d55ffcca14175.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,9 @@ export default function RootLayout({
     <html lang="en">
     <link
       rel="stylesheet"
-      href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-      crossOrigin=""
-    /> 
-    <link
-      rel="stylesheet"
       href="https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.css"
     /> 
+
     <body>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,5 @@
-'use client';
+import React from 'react';
 
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 export default function Page() {
-  return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-    <MapContainer
-    center={{
-      lat: 18.37733398653396,
-      lng: -65.92457771301271,
-    }}
-    zoom={10}
-    style={{ 
-      height: '500px',
-      width: '800px',
-      margin: 'auto',
-    }}
-    >
-    <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-    </div>
-  );
+  return <h1>Hello, Next.js!</h1>;
 }
-
