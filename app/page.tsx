4537238bf9c867c1c34fdd653394197179dc1ef5
Norbert M. Soto <norbert.soto@sunnova.com>
2024-07-23 18:14:46 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,24 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 
 export default function Page() {
   return (
-    <div style={{ height: '100vh', width: '100vw', display: 'flex'}}>
-    <MapContainer
-    center={{
-      lat: 18.37733398653396,
-      lng: -65.92457771301271,
-    }}
-    zoom={10}
-    style={{ 
-      height: '700px',
-      width: '1500px',
-      margin: 'auto',
-    }}
-    >
-    <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <div style={{ height: '100vh', width: '100vw', display: 'flex' }}>
+      <MapContainer
+        center={{
+          lat: 18.37733398653396,
+          lng: -65.92457771301271,
+        }}
+        zoom={10}
+        style={{
+          height: '700px',
+          width: '1500px',
+          margin: 'auto',
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   )
 };
